7c886a6e702e8701bca2610685e059212897959a
franpsif <franpsif@gmail.com>
2017-04-26 12:06:15 +0200

    Server changes needed for validation

---
+++ Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace ClassLibrary4
+{
+    public class Class1
+    {
+        private static readonly HttpClient client = new HttpClient();
+
+        public async System.Threading.Tasks.Task<HttpResponseMessage> CallServerAsync(String token_access)
+        {
+            //we need to use our own scope in order to know what is the scope secret (include that scope in the client definition)
+            //secret active until october: 9404762a-daee-4db2-8bf4-6cea1249b3eb
+            //scope: preveroemailscope
+
+            var plainTextBytes = System.Text.Encoding.UTF8.GetBytes("preveroemailscope:9404762a-daee-4db2-8bf4-6cea1249b3eb");
+            var auth = Convert.ToBase64String(plainTextBytes);
+
+            client.BaseAddress = new Uri("https://u4ids-sandbox.u4pp.com/identity/connect/introspect");
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", auth);
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            var values = new Dictionary<string, string>()
+            {
+                {"token", token_access}                
+            };
+            var content = new FormUrlEncodedContent(values);
+
+            HttpRequestMessage request = new HttpRequestMessage()
+            {
+                Method = HttpMethod.Post,
+                Content = content
+            };
+
+            return await client.SendAsync(request);
+        }
+    }
+}
---
+++ UnitTest1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibrary4;
+using System.Net.Http;
+using System.Web.Helpers;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Class1 class1 = new Class1();
+
+            System.Threading.Tasks.Task<HttpResponseMessage> response = class1.CallServerAsync("eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6IlBQNHJnN25uejh0OVdxX0lXRi1ORHd4eWM1TSIsImtpZCI6IlBQNHJnN25uejh0OVdxX0lXRi1ORHd4eWM1TSJ9.eyJpc3MiOiJodHRwczovL3U0aWRzLXNhbmRib3gudTRwcC5jb20vaWRlbnRpdHkiLCJhdWQiOiJodHRwczovL3U0aWRzLXNhbmRib3gudTRwcC5jb20vaWRlbnRpdHkvcmVzb3VyY2VzIiwiZXhwIjoxNDkzMTk2MjA4LCJuYmYiOjE0OTMxOTI2MDgsImNsaWVudF9pZCI6ImluZm9ybWF0aW9uLWJyb3dzZXIiLCJzY29wZSI6WyJvcGVuaWQiLCJwcmV2ZXJvZW1haWxzY29wZSJdLCJzdWIiOiJfcXIzd1FTS25salNkSS1KV244amRRUkQ5eFd3cmdLY0FldHFvVURCSVNzIiwiYXV0aF90aW1lIjoxNDkzMTkyNjA4LCJpZHAiOiJvcGVuaWRjb25uZWN0IiwidGVuYW50IjoicHJhZXRvcmlhbnMiLCJ1bml0NF9pZCI6Ik1hcmEiLCJhbXIiOlsiZXh0ZXJuYWwiXX0.pj-TWbimkl-6qvRPZfn-y9a0nmpLW9L7-IIgYxPeZv3aJwVX9NOJ0rE-6tsZe07H-eG2UFKCufNeSeV9fCgOrcDoPbvsYMysIC8hDNopDcpx_tzM7rPaaV5FCFXST64x1US92FD2YQYtHieVcMqlj_Bv9P_wDtwv7uGTLxc6ujYOKpUUjCRpgiMzcTkTnTrYT4ftjxV85Pr56QenHDJR_kfl7TK4xXdOZB7TKNVjbo0etEwXSfjqmZLqSKJdPdoF6Ct_E-ewv508arNCMPQ1-LyBFU5YQ4NS0H1hQFON26-1RKeHRkulOApVLFp4_W5neOYhuyrsO3qxsj_o0Sg9CA");
+            while (!response.IsCompleted) { }
+
+            string result = response.Result.Content.ReadAsStringAsync().Result;
+
+            dynamic decodedResponse = Json.Decode(result);
+
+            if (decodedResponse.Active)
+            {
+                Console.WriteLine("Active token");
+            }
+            else
+            {
+                Console.WriteLine("Inactive token");
+            }
+        }
+    }
+}
